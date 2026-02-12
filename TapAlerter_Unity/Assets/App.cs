9d1d7e823f7a5b5d1c5ebcd485582fbde6f3b928
surmwill <william.surmak@gmail.com>
2026-02-12 17:42:17 -0500

    adding push notification

--- TapAlerter_Unity/Assets/App.cs
+++ TapAlerter_Unity/Assets/App.cs
@@ -1,12 +1,11 @@
+#define USE_EMULATOR
+
 using System;
-using System.Collections;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using Firebase.Extensions;
+using System.IO;
 using Firebase.Firestore;
+using Firebase.Messaging;
 using TMPro;
 using UnityEngine;
-using UnityEngine.Networking;
 using UnityEngine.UI;
 
 public class App : MonoBehaviour
@@ -27,51 +26,66 @@ public class App : MonoBehaviour
     private Image _background = null;
 
     [SerializeField]
-    private Button _clearButton = null;
-
-    [SerializeField]
     private Button _refreshButton = null;
+    
+    private const string ConfigFileName = "config.json";
 
-    [SerializeField]
-    private Button _testGetWeatherButton = null;
+    private ListenerRegistration _listener;
+    
+    private string _userDocName;
     
     private void Start()
     {
-        _refreshButton.onClick.AddListener(UpdateTemperature);
-        _testGetWeatherButton.onClick.AddListener(TestGetWeather);
+        _userDocName = JsonUtility.FromJson<Config>(File.ReadAllText(Path.Combine(Application.streamingAssetsPath, ConfigFileName))).USER_DOC_GUID;
         
-        UpdateTemperature();
+        #if USE_EMULATOR
+        FirebaseFirestore db = FirebaseFirestore.DefaultInstance;
+        db.Settings.Host = "localhost:8080";
+        db.Settings.SslEnabled = false;
+        #endif
+        
+        _refreshButton.onClick.AddListener(Refresh);
+        
+        #if UNITY_ANDROID && !UNITY_EDITOR
+        FirebaseMessaging.MessageReceived += OnMessageReceived;
+        FirebaseMessaging.TokenReceived += OnTokenReceived;
+        #endif
+        
+        _listener = db.Collection("Users").Document(_userDocName).Listen(snapshot => UpdateUI(snapshot.ConvertTo<UserData>()));
+    }
+    
+    private void OnTokenReceived(object sender, TokenReceivedEventArgs token) 
+    {
+        Debug.Log($"Token Received: {token.Token}");
+        FirebaseFirestore.DefaultInstance.Collection("Users").Document(_userDocName).UpdateAsync("Token", token.Token);
+        Refresh();
+    }
+
+    private void OnMessageReceived(object sender, MessageReceivedEventArgs e) 
+    {
+        // Empty
+        Debug.Log("Message Received");
     }
 
     private void OnDestroy()
     {
-        _refreshButton.onClick.RemoveListener(UpdateTemperature);
-        _testGetWeatherButton.onClick.RemoveListener(TestGetWeather);
+        _refreshButton.onClick.RemoveListener(Refresh);
+        
+        #if UNITY_ANDROID && !UNITY_EDITOR
+        FirebaseMessaging.MessageReceived -= OnMessageReceived;
+        FirebaseMessaging.TokenReceived -= OnTokenReceived;
+        #endif
+        
+        _listener.Dispose();
     }
 
-    private void OnApplicationPause(bool pauseStatus)
+    private void Refresh()
     {
-        if (!pauseStatus)
-        {
-            UpdateTemperature();
-        }
+        FirebaseFirestore.DefaultInstance.Collection("Users").Document(_userDocName).UpdateAsync("Dirty", true);
     }
 
-    private void UpdateTemperature()
+    private void UpdateUI(UserData userData)
     {
-        UpdateTemperatureAsync().ContinueWithOnMainThread(t =>
-        {
-            if (!t.IsCompletedSuccessfully)
-            {
-                Debug.LogError($"Failed to update temperature: {t.Exception?.InnerException}");
-            }
-        });
-    }
-
-    private async Task UpdateTemperatureAsync()
-    {
-        UserData userData = await FetchUserAsync();
-
         _tomorrowsLow.text = $"Tommorow's low: {userData.TomorrowsLow}";
         _tomorrowsDate.text = $"Tomorrow's date: {userData.TomorrowsDate}";
         _warningTemperature.text = $"Warning temperature: {userData.WarningTemperature}";
@@ -79,44 +93,19 @@ public class App : MonoBehaviour
         _background.color = userData.TomorrowsLow <= userData.WarningTemperature ? Color.red : Color.green;
         _message.text = userData.TomorrowsLow <= userData.WarningTemperature ? "Turn on the taps!" : "Taps are okay!";
     }
-    
-    private async Task<UserData> FetchUserAsync()
-    {
-        DocumentSnapshot document = await FirebaseFirestore.DefaultInstance.Collection("Users").Document("User").GetSnapshotAsync();
-        return document.ConvertTo<UserData>();;
-    }
 
-    private void TestGetWeather()
+    [Serializable]
+    private class Config
     {
-        StartCoroutine(TestGetWeatherCoroutine());
-    }
-    
-    private IEnumerator TestGetWeatherCoroutine(float latitude = 45.5019f, float longitude = -73.561668f)
-    {
-        using (UnityWebRequest webRequest = UnityWebRequest.Get($"https://api.open-meteo.com/v1/forecast?latitude={latitude}&longitude={longitude}&daily=temperature_2m_min&timezone=auto"))
-        {
-            yield return webRequest.SendWebRequest();
-
-            if (webRequest.result == UnityWebRequest.Result.Success)
-            {
-                ForecastData data = JsonUtility.FromJson<ForecastData>(webRequest.downloadHandler.text);
-                if (data.daily?.temperature_2m_min.Count >= 2)
-                {
-                    float tomorrowLow = data.daily.temperature_2m_min[1];
-                    string tomorrowDate = data.daily.time[1];
-                    Debug.Log($"Tomorrow's Low ({tomorrowDate}): {tomorrowLow}°C");
-                }
-            }
-            else
-            {
-                Debug.LogError($"Weather request failed: {webRequest.error}");
-            }
-        }
+        public string USER_DOC_GUID;
     }
     
     [FirestoreData]
     private class UserData
     {
+        [FirestoreProperty]
+        public bool Dirty { get; set; }
+        
         [FirestoreProperty]
         public float Latitude { get; set; }
         
@@ -131,22 +120,8 @@ public class App : MonoBehaviour
         
         [FirestoreProperty]
         public float WarningTemperature { get; set; }
+        
+        [FirestoreProperty]
+        public string Token { get; set; }
     }
-    
-    #region Weather_Parsing
-    
-    [Serializable]
-    public class ForecastData
-    {
-        public DailyData daily;
-    }
-
-    [Serializable]
-    public class DailyData
-    {
-        public List<string> time;
-        public List<float> temperature_2m_min; 
-    }
-    
-    #endregion
 }
--- functions/index.js
+++ functions/index.js
@@ -1,14 +1,11 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
-
 const {setGlobalOptions} = require("firebase-functions");
 const {onRequest} = require("firebase-functions/https");
+const { onDocumentUpdated } = require("firebase-functions/v2/firestore");
+const { getFirestore } = require("firebase-admin/firestore");
+const { initializeApp } = require("firebase-admin/app");
+const { onSchedule } = require("firebase-functions/v2/scheduler");
+const admin = require("firebase-admin");
+const config = require("./config");
 const logger = require("firebase-functions/logger");
 
 // For cost control, you can set the maximum number of containers that can be
@@ -21,12 +18,68 @@ const logger = require("firebase-functions/logger");
 // functions should each use functions.runWith({ maxInstances: 10 }) instead.
 // In the v1 API, each function can only serve one request per container, so
 // this will be the maximum concurrent request count.
-setGlobalOptions({ maxInstances: 10 });
+setGlobalOptions({ maxInstances: 1 });
 
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
+// Initialize the Admin SDK once at the top
+initializeApp();
+const db = getFirestore();
 
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const userDocRef = db.collection("Users").doc(config.USER_DOC_GUID);
+
+exports.setUserDirtyScheduled = onSchedule("* * * * *", async (event) => {
+    console.log('ran');
+    await userDocRef.update({ Dirty: true });
+});
+
+// http://127.0.0.1:5001/tapalerter/us-central1/setUserDirtyManual
+exports.setUserDirtyManual = onRequest(async (req, res) => {
+    await userDocRef.update({ Dirty: true });
+    res.status(200).send();
+});
+
+exports.userTrigger = onDocumentUpdated(`Users/${config.USER_DOC_GUID}`, async (event) => {
+    const user = event.data.after.data();
+    if (!user.Dirty)
+    {
+        return;
+    }
+
+    const response = await fetch(`https://api.open-meteo.com/v1/forecast?latitude=45.5019&longitude=-73.561668&daily=temperature_2m_min&timezone=auto`);
+    if (!response.ok) {
+        console.log(`Error fetching weather. Status: ${response.status}`);
+        return;
+    }
+
+    const data = await response.json();
+    const tomorrowsDate = data.daily.time[1];
+    const tomorrowsLow = data.daily.temperature_2m_min[1];
+    
+    await userDocRef.update({
+        TomorrowsLow: tomorrowsLow,
+        TomorrowsDate: tomorrowsDate,
+        Dirty: false
+    });
+
+    console.log(tomorrowsDate, tomorrowsLow);
+
+    const isEmulator = process.env.FUNCTIONS_EMULATOR === 'true';
+    if (!user.Token || isEmulator)
+    {
+        return;
+    }
+
+    const message = {
+        notification: {
+            title: "Freezing Alert!",
+            body: `Tomorrow's low is ${tomorrowsLow}°C. Turn on the taps!`,
+        },
+        token: user.Token
+    };
+
+    try {
+        await admin.messaging().send(message);
+        console.log("Notification sent successfully");
+    } catch (error) {
+        console.error("Error sending notification:", error);
+    }
+});
